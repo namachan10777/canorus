fad8d03d3f14eda1bd7b866cac58b04ae4a23fee
Nakano Masaki <namachan10777@gmail.com>
2020-02-16 17:48:44 +0900

    draft 2nd parser

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,40 @@ extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 use pest::Parser;
+
+struct header {
+    description: String,
+    implementation_level: String,
+    name: String,
+    author: String,
+    organization: String,
+    preprocessor_version: String,
+    originating_system: String,
+    authorisation: String,
+    file_schema: String,
+}
+
+type v3 = (f64, f64, f64);
+
+#[derive(Debug)]
+pub struct axis {
+    p: v3,
+    axis1: v3,
+    axis2: v3,
+}
+
+#[derive(Debug)]
+pub enum face_element {
+    Cylinder(f64, axis),
+    Plane(axis),
+}
+
+#[derive(Debug)]
+pub struct advanced_face {
+    flag: bool,
+    elem: face_element,
+}
+
+pub fn parse(s : &str) -> Vec<advanced_face> {
+    Vec::new()
+}
